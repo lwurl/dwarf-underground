4c2e82a2f9ff3bda02151869835e6d8bf333f0a3
Luke Wurl <lwurl@nd.edu>
2018-05-22 00:22:19 -0400

    Create header component

--- src/Header.js
+++ src/Header.js
@@ -3,22 +3,24 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div class="expanded row header">
-                <div class="clearfix">
-                    <div class="float-left logo">
+            <div className="expanded row header">
+                <div className="clearfix">
+                    <div className="float-left logo">
                         <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
                     </div>
-                    <div class="float-right nav-links">
-                        <a href="#">Home</a>
-                        <a href="#">About</a>
-                        <a href="#">Log in</a>
+                    <div className="float-right nav-links">
+                        <a href="">Home</a>
+                        <a href="">About</a>
+                        <a href="">Log in</a>
                     </div>
                 </div>
-                <div class="headline text-center small-12 columns">
+                <div className="headline text-center small-12 columns">
                     <h1>The Dwarf Underground</h1>
-                    <h3>Delving deep for the <span class="gold">shiniest</span> news</h3>
+                    <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
                 </div>
             </div>
-        )
+        );
     }
 }
+
+export default Header;
