8e41dfe93945416020dc0f19a4fea703b67a60c9
Luke Wurl <lwurl@nd.edu>
2018-05-22 09:50:06 -0400

    Add ArticleInfo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import Header from './Header';
+import ArticleInfo from './ArticleInfo'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <ArticleInfo />
+      </div>
     );
   }
 }
